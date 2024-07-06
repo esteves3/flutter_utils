71613c783f3fc9776836b9474d22881f9fee2d96
Esteves <46036163+esteves3@users.noreply.github.com>
2024-07-06 20:28:11 +0100

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 const kDefaultBorderRadius = 24.0;
-const kDefaultBorderRadiusAllCircular =
+const defaultBorderRadius =
     BorderRadius.all(Radius.circular(kDefaultBorderRadius));
 const kDefaultPadding = 14.0;
+const defaultPadding = EdgeInsets.all(kDefaultPadding);
 
 const kDefaultAnimationDuration = Duration(milliseconds: 300);
 const kMobileMaxWidth = 850;
