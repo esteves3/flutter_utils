d60bf8aefc6757bd0c31c29d8865d097f3ba0696
Carlos Esteves <cmdesteves@gmail.com>
2024-07-07 19:01:49 +0100

    Add zoom in on hover effect component

---
+++ lib/components/hover_zoom_in.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HoverZoomIn extends StatefulWidget {
+  final Duration? duration;
+  final double zoom;
+  final Widget? child;
+  final bool? isHovered;
+  const HoverZoomIn(
+      {super.key, this.zoom = 1.1, this.child, this.duration, this.isHovered});
+
+  @override
+  State<HoverZoomIn> createState() => _HoverZoomInState();
+}
+
+class _HoverZoomInState extends State<HoverZoomIn> {
+  var isHovered = false;
+
+  @override
+  void initState() {
+    isHovered = widget.isHovered ?? false;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: widget.duration ?? const Duration(milliseconds: 300),
+      transform: Matrix4.identity()
+        //..translate(widget.zoomOffset?.dx ?? 0.0, widget.zoomOffset?.dy ?? 0.0)
+        ..scale(isHovered ? widget.zoom : 1),
+      child: InkWell(
+        child: widget.child,
+        onHover: (value) {
+          if (widget.isHovered != null) {
+            setState(() {
+              isHovered = value;
+            });
+          }
+        },
+      ),
+    );
+  }
+}
